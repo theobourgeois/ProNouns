ffbef8020d459fe13db730d7ffb738dc718f3810
theobourgeois <theoobourgeois@gmail.com>
2022-12-30 12:06:13 -0400

    adjusted height of image container on mobile view

--- client/src/Components/Image/Image.jsx
+++ client/src/Components/Image/Image.jsx
@@ -6,7 +6,7 @@ function Image() {
   const { image } = useContext(ImageContext)
 
   return (
-    <div style={{transform: "translateY(-50px)", border: "0.5px solid black"}} className="overflow-hidden flex justify-center items-center bg-gray-300 w-full h-full shadow-inner border-8 border-gray-200 border-solid">
+    <div style={{transform: "translateY(-50px)", border: "0.5px solid black"}} className="overflow-hidden flex justify-center items-center bg-gray-300 w-full sm:h-full shadow-inner border-8 border-gray-200 border-solid">
       <img className="object-contain w-5/6 shadow-lg border-4 border-white border-solid" src={image} alt="guess"></img>
     </div>
   );
